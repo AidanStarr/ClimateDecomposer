8d5a315b6a00dabab50948c84fe6e6554987c904
Aidan Starr <aidan.m.starr@gmail.com>
2023-05-07 18:12:19 +0100

    Added select_slider to input multiple RCs (and sum the result). Also allow detrending before periodogram

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import streamlit as st
 import plotly.express as px
+import numpy as np
 from src import util
 
 st.set_page_config(page_title = "ClimateDecomposer", page_icon="~")
 
-st.title("~ClimateDecomposer")
-st.markdown('### Decomposing the paleoclimate record into orbital components using Singular Spectrum Analysis')
+st.title("ClimateDecomposer")
+st.markdown('##### :earth_africa: Decompose paleoclimate records using *Singular Spectrum Analysis* (SSA)  :earth_africa:')
 
-about = '''ClimateDecomposer allows you to decompose famous paleoclimate records into their constituent components and noise. In other words, it lets you visualise the different components of past climates using *Singular Spectrum Analysis* (SSA), in particular the orbital cycles underlying long-term climate records. A periodogram of each Reconstructed Components can then be viewed to check if it aligns with a known orbital frequency band.  
+about = '''How to use:
+1. Choose a paleoclimate record to decompose from the dropdown menu in the sidebar
+2. Choose a **window length** to configure the SSA decomposition
+3. Select and view the **Reconstructed Components**: Low RCs (1 to ~10) will usually show the more important components, while high RCs are often noise
+4. View the periodogram from the selected RC to check if it aligns with known orbital frequencies.
 
-Created and maintained by [Aidan Starr](https://aidanstarr.github.io/). SSA code adapted from [Braun et al., 2023](https://doi.org/10.1038/s43247-023-00717-5) and [this blog post by Jordon D\'Arcy](https://www.kaggle.com/code/jdarcy/introducing-ssa-for-time-series-decomposition/notebook). The app itself is inspired by [**Renewcast** by Giannis Tolios](http://renewcast.giannis.io/) - check it out.  
-
-A primer on SSA for paleoclimate time series can be found [in this paper by Vautard and Ghil](https://doi.org/10.1016/0167-2789(89)90077-8). For now, try different window sizes (L) using the slider on the left, and see what the different Reconstructed Components are like! 
+Created and maintained by [Aidan Starr](https://aidanstarr.github.io/). SSA code adapted from [Braun et al., 2023](https://doi.org/10.1038/s43247-023-00717-5) and [this blog post by Jordon D\'Arcy](https://www.kaggle.com/code/jdarcy/introducing-ssa-for-time-series-decomposition/notebook). The app itself is inspired by [**Renewcast** by Giannis Tolios](http://renewcast.giannis.io/) - check it out.
 '''
 
 st.markdown(about)
 
+expander = st.expander(":chart_with_upwards_trend: SSA Primer :chart_with_upwards_trend:")
+expander.write('''coming soon....
+A more comprehensive primer on SSA can be found [in this paper by Vautard and Ghil](https://doi.org/10.1016/0167-2789(89)90077-8).
+''')
 
 ### sidebar
 series = st.sidebar.selectbox(label = "Select a paleo-climate time series",
@@ -25,7 +32,7 @@ container_cat = st.sidebar.container()
 
 with container_cat:
     L = st.sidebar.slider(label = 'Window Size (L)',
-                                         min_value = 2, max_value = 600, value = 200)    
+                                         min_value = 2, max_value = 600, value = 200)
 
 
 #### plot the data
@@ -33,13 +40,13 @@ st.markdown('---- \n \n \n')
 if series == 'Benthic Stack':
     ylabel = 'δ18O (‰)'
     destex = '#### Data: The Global Benthic δ18O stack of [Ahn et al., 2017](https://doi.org/10.1093/climsys/dzx002)'
-    
+
 elif series == 'Ice Core CO2':
     ylabel = 'CO2 (ppm)'
     destex = '#### Data: Composite record of atmospheric CO2 trapped in Antarctic ice cores from [Bereiter et al., 2016](https://doi.org/10.1002/2014GL061957) and references therein'
 
 st.markdown(destex)
-    
+
 df = util.get_data(series)
 fig = px.line(x=df.index, y=df.data)
 fig.update_traces(line_color='#ef476f')
@@ -50,24 +57,39 @@ st.plotly_chart(fig, use_container_width = True)
 #### SSA
 ssa = util.SSA(df['data'], L=L)
 
-
-with container_cat:
-        RC_num = st.selectbox(label = 'Reconstructed Component', options = range(1,L))   
-
 st.markdown('#### Reconstructed Components from SSA')
 
-fig2 = px.line(x=ssa.reconstruct([0]).index, y=ssa.reconstruct([RC_num-1]),title='Reconstructed Component '+str(RC_num))
+
+with container_cat:
+        RC_num0,RC_num1 = st.select_slider(label = 'Reconstructed Component', options = range(1,40),value=[1,1])
+        if RC_num0==RC_num1:
+            RC_num = RC_num0
+        else:
+            RC_num = list([RC_num0,RC_num1])
+
+if isinstance(RC_num,int):
+    y = ssa.reconstruct(RC_num-1)
+    titl = str(RC_num)
+else:
+    y = ssa.reconstruct(np.arange(RC_num[0]-1,RC_num[1]-1,1))
+    titl = '['+str(RC_num0)+':'+str(RC_num1)+']'
+fig2 = px.line(x=ssa.reconstruct([0]).index, y=y,title='Reconstructed Component '+titl)
 fig2.update_traces(line_color='#ef476f')
 fig2.update_layout(xaxis_title='Time Before Present (kyr))', yaxis_title=ylabel)
 fig2.update_layout(title=dict(font=dict(size=20), y=1,x=0.6, yref='paper'))
 st.plotly_chart(fig2, use_container_width = True)
 
 #### periodogram
-f,pxx = util.periodogram(ssa.reconstruct([0]).index,ssa.reconstruct([RC_num-1]))
-
 st.markdown('#### Periodogram of Reconstructed Components')
 
-fig3 = px.line(x=f, y=pxx, title='Reconstructed Component '+str(RC_num))
+detrend = st.checkbox('Detrend?')
+if detrend:
+    y = y - ssa.reconstruct([0])
+
+f,pxx = util.periodogram(ssa.reconstruct([0]).index,y)
+
+
+fig3 = px.line(x=f, y=pxx, title='Reconstructed Component '+titl)
 fig3.update_traces(line_color='#ef476f')
 fig3.update_layout(xaxis_title='Frequency (1/kyr)', yaxis_title='Spectral Power')
 fig3.update_xaxes(range=[0, 0.12], row=1, col=1)
@@ -77,10 +99,9 @@ fig3.add_vrect(x0=1/120, x1=1/90,line_width=0, fillcolor="#dde5b6", opacity=0.1,
 fig3.add_vrect(x0=1/46, x1=1/35,line_width=0, fillcolor="#dde5b6", opacity=0.1,annotation_text="Obliquity", annotation_position="outside top")
 fig3.add_vrect(x0=1/25, x1=1/19,line_width=0, fillcolor="#dde5b6", opacity=0.1,annotation_text="Precession", annotation_position="outside top")
 
-fig3.update_layout(title=dict(font=dict(size=20), y=1,x=0.6, yref='paper'))
+fig3.update_layout(title=dict(font=dict(size=20), y=0.9,x=0.55, yref='paper'))
 st.plotly_chart(fig3, width = 50)
 
 
-##### 
+#####
 # with st.expander('More plots, please!'):
-    
--- src/util.py
+++ src/util.py
@@ -1,9 +1,9 @@
 # Standard library imports
 import numpy as np
-import pandas as pd    
+import pandas as pd
 import random
-import time 
-import scipy   
+import time
+import scipy
 from scipy.interpolate import interp1d
 from scipy import signal
 
@@ -17,7 +17,7 @@ def get_data(name):
         df_ = pd.DataFrame(index=t_new)
         df_['data'] = interp1d(df.index.values,df['d18o'])(t_new)
         df_['std'] = interp1d(df.index.values,df['std'])(t_new)
-    
+
     elif name=='Ice Core CO2':
         url = 'https://www.ncei.noaa.gov/pub/data/paleo/icecore/antarctica/antarctica2015co2composite.txt'
         df = pd.read_csv(url,header=137,sep='\t')
@@ -26,10 +26,10 @@ def get_data(name):
         df_ = pd.DataFrame(index=t_new)
         df_['data'] = interp1d(df['age_gas_calBP'],df['co2_ppm'])(t_new)
         df_['std'] = interp1d(df['age_gas_calBP'],df['co2_1s_ppm'])(t_new)
-        
+
     return df_
 
-def next_power_of_2(x):  
+def next_power_of_2(x):
     return 1 if x == 0 else 2**(x - 1).bit_length()
 
 def periodogram(x,y):
@@ -37,58 +37,58 @@ def periodogram(x,y):
     f,Pxx = signal.periodogram(y,fs=fs,nfft=next_power_of_2(len(y)))
     return f,Pxx
 
-    
+
 class SSA(object):
-    """ 
-    SOURCE: 
-        - This code is taken directly from Braun et al., 2023: https://www.nature.com/articles/s43247-023-00717-5 
-        - All credit to Braun et al., for the excellent paper and great code! 
+    """
+    SOURCE:
+        - This code is taken directly from Braun et al., 2023: https://www.nature.com/articles/s43247-023-00717-5
+        - All credit to Braun et al., for the excellent paper and great code!
         - The code itself was accessed via https://zenodo.org/record/7104976#.ZEvKvuxuerc
-        - Braun cites this blog as the original source: https://www.kaggle.com/code/jdarcy/introducing-ssa-for-time-series-decomposition/notebook 
-        
+        - Braun cites this blog as the original source: https://www.kaggle.com/code/jdarcy/introducing-ssa-for-time-series-decomposition/notebook
+
     """
 
-    
+
     __supported_types = (pd.Series, np.ndarray, list)
-    
+
     def __init__(self, tseries, L, save_mem=True):
         """
         Decomposes the given time series with a singular-spectrum analysis. Assumes the values of the time series are
         recorded at equal intervals.
 
         """
-        
+
         # Tedious type-checking for the initial time series
         if not isinstance(tseries, self.__supported_types):
             raise TypeError("Unsupported time series object. Try Pandas Series, NumPy array or list.")
-        
+
         # Checks to save us from ourselves
         self.N = len(tseries)
         if not 2 <= L <= self.N/2:
             raise ValueError("The window length must be in the interval [2, N/2].")
-        
+
         self.L = L
         self.orig_TS = pd.Series(tseries)
         self.K = self.N - self.L + 1
-        
+
         # Embed the time series in a trajectory matrix
         self.X = np.array([self.orig_TS.values[i:L+i] for i in range(0, self.K)]).T
-        
+
         # Decompose the trajectory matrix
         self.U, self.Sigma, VT = np.linalg.svd(self.X)
         self.d = np.linalg.matrix_rank(self.X)
-        
+
         self.TS_comps = np.zeros((self.N, self.d))
-        
+
         if not save_mem:
             # Construct and save all the elementary matrices
             self.X_elem = np.array([ self.Sigma[i]*np.outer(self.U[:,i], VT[i,:]) for i in range(self.d) ])
 
-            # Diagonally average the elementary matrices, store them as columns in array.           
+            # Diagonally average the elementary matrices, store them as columns in array.
             for i in range(self.d):
                 X_rev = self.X_elem[i, ::-1]
                 self.TS_comps[:,i] = [X_rev.diagonal(j).mean() for j in range(-X_rev.shape[0]+1, X_rev.shape[1])]
-            
+
             self.V = VT.T
         else:
             # Reconstruct the elementary matrices without storing them
@@ -96,15 +96,15 @@ class SSA(object):
                 X_elem = self.Sigma[i]*np.outer(self.U[:,i], VT[i,:])
                 X_rev = X_elem[::-1]
                 self.TS_comps[:,i] = [X_rev.diagonal(j).mean() for j in range(-X_rev.shape[0]+1, X_rev.shape[1])]
-            
+
             self.X_elem = "Re-run with save_mem=False to retain the elementary matrices."
-            
+
             # The V array may also be very large under these circumstances, so we won't keep it.
             self.V = "Re-run with save_mem=False to retain the V matrix."
-        
+
         # Calculate the w-correlation matrix.
         self.calc_wcorr()
-            
+
     def components_to_df(self, n=0):
         """
         Returns all the time series components in a single Pandas DataFrame object.
@@ -113,41 +113,39 @@ class SSA(object):
             n = min(n, self.d)
         else:
             n = self.d
-        
+
         # Create list of columns - call them F0, F1, F2, ...
         cols = ["F{}".format(i) for i in range(n)]
         return pd.DataFrame(self.TS_comps[:, :n], columns=cols, index=self.orig_TS.index)
-            
-    
+
+
     def reconstruct(self, indices):
         """
-        Reconstructs the time series from its elementary components, using the given indices. 
+        Reconstructs the time series from its elementary components, using the given indices.
         """
         if isinstance(indices, int): indices = [indices]
-        
+
         ts_vals = self.TS_comps[:,indices].sum(axis=1)
         return pd.Series(ts_vals, index=self.orig_TS.index)
-    
+
     def calc_wcorr(self):
         """
         Calculates the w-correlation matrix
         """
-             
+
         # Calculate the weights
         w = np.array(list(np.arange(self.L)+1) + [self.L]*(self.K-self.L-1) + list(np.arange(self.L)+1)[::-1])
-        
+
         def w_inner(F_i, F_j):
             return w.dot(F_i*F_j)
-        
+
         # Calculated weighted norms, ||F_i||_w, then invert.
         F_wnorms = np.array([w_inner(self.TS_comps[:,i], self.TS_comps[:,i]) for i in range(self.d)])
         F_wnorms = F_wnorms**-0.5
-        
+
         # Calculate Wcorr.
         self.Wcorr = np.identity(self.d)
         for i in range(self.d):
             for j in range(i+1,self.d):
                 self.Wcorr[i,j] = abs(w_inner(self.TS_comps[:,i], self.TS_comps[:,j]) * F_wnorms[i] * F_wnorms[j])
                 self.Wcorr[j,i] = self.Wcorr[i,j]
-    
-    
